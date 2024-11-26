e50d874edfae04e2ec8df7c92032aa09d9c7a625
trungtt6 <trungtt6@ocb.com.vn>
2024-11-26 11:30:59 +0700

    rename of model

--- src/main/java/com/example/model/ElasticsearchSinkModel.java
+++ src/main/java/com/example/model/ElasticsearchSinkModel.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class OcbElasticsearchSinkModel<T> {
+public class ElasticsearchSinkModel<T> {
     
     public String indexName;
 
